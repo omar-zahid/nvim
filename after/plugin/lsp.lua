62eaca9054bcba3ec5a23993db22a02c1eb3d7ae
Omar Zahid <omarzahid@outlook.com>
2023-12-31 23:36:45 +0800

    Added more servers on ensure installed

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -49,11 +49,11 @@ end
 --  Add any additional override configuration in the following tables. They will be passed to
 --  the `settings` field of the server config. You must look up that documentation yourself.
 local servers = {
-	-- clangd = {},
-	-- gopls = {},
-	-- pyright = {},
-	-- rust_analyzer = {},
-	-- tsserver = {},
+	emmet_language_server = {},
+	docker_compose_language_service = {},
+	rust_analyzer = {},
+	tsserver = {},
+	eslint = {},
 
 	lua_ls = {
 		Lua = {
