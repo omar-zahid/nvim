5b3556318a00c17a1675ba726376586424b58398
Omar Zahid <omarzahid@outlook.com>
2024-09-21 12:56:17 +0800

    Update harpoon keymap and icons fix

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -5,7 +5,7 @@ harpoon:setup()
 -- REQUIRED
 
 vim.keymap.set("n", "<leader>af", function()
-	harpoon:list():append()
+	harpoon:list():add()
 end)
 vim.keymap.set("n", "<C-i>", function()
 	harpoon.ui:toggle_quick_menu(harpoon:list())
--- lua/omar-zahid/lazy.lua
+++ lua/omar-zahid/lazy.lua
@@ -135,8 +135,8 @@ require("lazy").setup({
 			},
 		},
 		-- Optional dependencies
-		dependencies = { { "echasnovski/mini.icons", opts = {} } },
-		-- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
+		-- dependencies = { { "echasnovski/mini.icons", opts = {} } },
+		dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
 	},
 	{
 		"kylechui/nvim-surround",
