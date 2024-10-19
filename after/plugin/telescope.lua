5474bf6c8d6c94bd75f64fb44abd96ce23749891
Omar Zahid <omarzahid@outlook.com>
2024-10-19 16:30:24 +0800

    Added smart open

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,11 +1,5 @@
 require("telescope").setup({
 	defaults = {
-		mappings = {
-			i = {
-				["<C-u>"] = false,
-				["<C-d>"] = false,
-			},
-		},
 		layout_strategy = "vertical",
 	},
 })
@@ -15,7 +9,6 @@ pcall(require("telescope").load_extension, "fzf")
 
 -- See `:help telescope.builtin`
 vim.keymap.set("n", "<leader>?", require("telescope.builtin").oldfiles, { desc = "[?] Find recently opened files" })
-vim.keymap.set("n", "<leader><space>", require("telescope.builtin").buffers, { desc = "[ ] Find existing buffers" })
 vim.keymap.set("n", "<leader>/", function()
 	-- You can pass additional configuration to telescope to change theme, layout, etc.
 	require("telescope.builtin").current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
@@ -29,3 +22,7 @@ vim.keymap.set("n", "<leader>sh", require("telescope.builtin").help_tags, { desc
 vim.keymap.set("n", "<leader>sw", require("telescope.builtin").grep_string, { desc = "[S]earch current [W]ord" })
 vim.keymap.set("n", "<leader>sg", require("telescope.builtin").live_grep, { desc = "[S]earch by [G]rep" })
 vim.keymap.set("n", "<C-p>", require("telescope.builtin").git_files, { desc = "Search Git Files" })
+-- vim.keymap.set("n", "<leader><space>", require("telescope.builtin").buffers, { desc = "[ ] Find existing buffers" })
+vim.keymap.set("n", "<leader><leader>", function()
+	require("telescope").extensions.smart_open.smart_open()
+end, { noremap = true, silent = true })
