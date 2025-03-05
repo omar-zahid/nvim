c5d158de80c91174105b7a5ff57dc52666e89e4a
Omar Zahid <omarzahid@outlook.com>
2025-03-05 17:19:26 +0800

    Removed http treesitter from ensured installed

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -3,7 +3,7 @@
 -- See `:help nvim-treesitter`
 require("nvim-treesitter.configs").setup({
 	-- Add languages to be installed here that you want installed for treesitter
-	ensure_installed = { "lua", "rust", "tsx", "typescript", "vimdoc", "vim", "http" },
+	ensure_installed = { "lua", "rust", "tsx", "typescript", "vimdoc", "vim" },
 
 	-- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
 	auto_install = true,
